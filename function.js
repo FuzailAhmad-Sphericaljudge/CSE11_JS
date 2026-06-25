63620c428705ee68172dfdf5cfcf751dd39488ca
FuzailAhmad-Sphericaljudge <25f2008733@ds.study.iitm.ac.in>
2026-06-25 10:13:18 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,5 @@
+function myFunction(){
+
+}
+alert("Hello World");
+<button onclick="myFunction()">try it</button>
---
+++ while.js
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        </body></html><script type="text/javascript">
+var count = 0;
+ document.write("Starting Loop<br/>");
+
+while(count < 10) {
+    document.write(count + "<br>");
+    count++;
+}
+</script>
+</body>
+</html>
